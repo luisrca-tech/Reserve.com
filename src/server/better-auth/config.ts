389a19df96106ec54865d3fc3745a35c91215b2d
Luis Felipe <luisrochacruzalves@gmail.com>
2026-05-15 15:26:32 -0300

    Enhance: Update Home component layout and improve authentication forms

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -1,10 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 
-import { env } from "~/env";
 import { db } from "~/server/db";
 
 export const auth = betterAuth({
+  plugins: [nextCookies()],
   database: drizzleAdapter(db, {
     provider: "pg", // or "pg" or "mysql"
   }),
